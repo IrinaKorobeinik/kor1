7c5e82785be368a0d832281cfc05bd010f2dfa50
IrinaKorobeinik <korsksksk@gmail.com>
2015-11-05 01:52:45 +0200

    kor01

---
+++ kor_lab0/kor_lab0/kor_lab0.cpp
@@ -0,0 +1,15 @@
+// kor_lab0.cpp: определяет точку входа для консольного приложения.
+//
+
+#include "stdafx.h"
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    
+	cout << "MEM" << endl;
+	return 0;
+}
+
